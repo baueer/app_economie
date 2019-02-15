59f8032693e032cb4c39208145c79e242c973aee
baueer <38387044+baueer@users.noreply.github.com>
2019-02-15 19:54:18 +0200

    version 1.0

---
+++ src/data.js
@@ -0,0 +1,226 @@
+var data = [
+    // 1
+    ["Reprezinta forme distincte de economie, economia:", 
+        "Naturala si de piata", 
+        "Naturala, de schimb si de piata", 
+        "Naturala si de schimb", 
+        "Numai autoconsumul",
+    3],
+    // 2
+    ["Economia de piata:", 
+        "Se bazeaza pe proprietatea comuna asupra mijloacelor de productie", 
+        "Raspunde liber la intrebarile: ce?, cat?, cum?, pentru cine? sa produca", 
+        "Presupune ca piata sa fie dirijata", 
+        "Preturile administrate sunt predominante pe piata", 
+    2],
+    // 3
+    ["Care din urmatoarele situatii ilustreaza autoconsumul?",
+        "Folosirea unui bun pentru a obtine in schimb un alt bun",
+        "Utilizarea unui imprumut pentru a cumpara un automobil",
+        "Utilizarea rosiilor din productia proprie pentru hrana familiei",
+        "Utilizarea bunurilor cumparate cu venituri proprii",
+    3],
+    // 4
+    ["Autonomia economica a producatorilor este:",
+        "Bazata pe proprietatea privata asupra mijloacelor de productie",
+        "Caracteristica economiei naturale",
+        "Bazata pe proprietatea comuna asupra mijloacelor de productie",
+        "Denumita si interventia statului in economie",
+    1],
+    // 5
+    ["Reprezinta o trasatura a economiei de piata:",
+        "Desfasurarea activitatii economice pe baza de plan",
+        "Administrarea preturilor numai de catre stat",
+        "Faptul ca bunurile imbraca forma de marfa",
+        "Centralizarea activitatii economice",
+    3],
+    // 6
+    ["In economia de piata, satisfacerea nevoilor are loc:",
+        "Numai prin autoconsum",
+        "Prin producerea bunurilor necesare de fiecare consumator",
+        "Prin repartizarea echitabila a bunurilor",
+        "Prin intermediul schimbului",
+    4],
+    // 7
+    ["Marfa:",
+        "Este un produs al muncii omenesti care satisface o nevoie",
+        "Reprezinta cerinte obiective ale vietii",
+        "Este realizata prin autoconsum",
+        "Este impulsul initial si fundamental al activitatii economice",
+    1],
+    // 8
+    ["In sistemul economic marfar, relatiile dintre agentii economici sunt:",
+        "Organizate si administrate de stat",
+        "Determinate de interese obstesti",
+        "Relatii concurentiale",
+        "Multiplicate prin impunerea unor bariere tarifare si netarifare",
+    3],
+    // 9
+    ["Care dintre urmatoarele enunturi este o situatie eronata?",
+        "Sistemul economic concurential conduce la realizarea propriului interes",
+        "Interventia statului la nivel de activiate economico-sociala este puternica",
+        "Preturile bunurilor economice imbraca forma de marfa, in general",
+        "Simbolurile monetare asigura fluiditate schimburilor economice",
+    2],
+    // 10
+    ["In economia de piata, pe baza proprietatii private se asigura:",
+        "Diviziunea muncii",
+        "Libertatea economica",
+        "Completarea surselor de finantare a activitatii politice",
+        "Specializarea internationala a muncii",
+    2],
+    // 11
+    ["Proprietatea este contractul dintre oameni cu privire la:",
+        "Bunuri",
+        "Venituri",
+        "Cheltuieli",
+        "Tranzactii economice",
+    1],
+    // 12
+    ["Proprietarul neproducator beneficiaza de atributiile:",
+        "Posesie, dispozitie",
+        "Utilizare, uzufruct",
+        "Dispozitie, utilizare, uzufruct",
+        "Posesie, dispozitie, utilizare",
+    1],
+    // 13
+    ["Asupra utilajelor folosite, salariatul dintr-o intreprindere nu are:",
+        "Drept de posesie",
+        "Drept de utilizare",
+        "Drept de uzufruct",
+        "Drept de posesie si dispozitie",
+    4],
+    // 14
+    ["In proprietate individuala pot fi:",
+        "Micile unitati comerciale",
+        "Cooperativele de credit",
+        "Societatile pe actiuni",
+        "Intreprinderi lucrative de dimensiuni mari",
+    1],
+    // 15
+    ["Proprietatea mixta presupune asocierea agentilor economici:",
+        "Particulari, unii cu altii",
+        "Particulari cu statul",
+        "Particulari din diferite tari",
+        "Publici, unii cu altii",
+    2],
+    // 16
+    ["Coexistenta formelor de proprietate inseamna:",
+        "Pluralismul formelor de proprietate",
+        "Monopolul unei forme de proprietate",
+        "Asocierea formelor de proprietate in proprietatea mixta",
+        "Cooperarea institutiilor publice pentru asigurarea functionarii economiei de piata",
+    1],
+    // 17
+    ["Pluralismul proprietatii presupune:",
+        "Cresterea cheltuielilor de fabricare a bunurilor",
+        "Accelerarea procesului de uzura a utilajelor",
+        "Afirmarea eficientei absortiei inovatiei la nivelul fluxurilor tehnologice",
+        "Majorarea preturilor pietei",
+    3],
+    // 18
+    ["Libertatea economica presupune:",
+        "Decizii profitabile aplicate de toti agentii economici",
+        "Cresterea gradului de satisfactie a consumatorului",
+        "Implicarea masiva a statului in economie",
+        "Libertatea fundamentala a oricarui cetatean de a desfasura activitati economice",
+    4],
+    // 19
+    ["In economia de piata, intreprinderile dezvolta relatii cu statul prin:",
+        "Costuri",
+        "Economii de la alti agenti economici",
+        "Divizarea muncii",
+        "Plata impozitelor si primirea de subventii",
+    4],
+    // 20
+    ["In sistemul economic marfar, activitatea care nu are caracter comercial:",
+        "Furnizarea serviciilor de aparare nationala",
+        "Vanzarea de bunuri de consum final",
+        "Acordarea de credite clientilor bancari",
+        "Transferul unor sume de bani intre doua conturi bancare",
+    1],
+    // 21
+    ["Administrarea privata in sistemul economic capitalist este asociata cu:",
+        "Armata",
+        "Invatamantul public",
+        "Partidele politice",
+        "Primaria",
+    3],
+    // 22
+    ["Administratiile publice:",
+        "Realizeaza productie comerciala pentru menaje",
+        "Realizeaza productie necomerciala pentru colectivitati",
+        "Distribuie veniturile intre agentii economici",
+        "Realizeaza productie comerciala pentru toti agentii economici",
+    2],
+    // 23
+    ["Bancile, ca unitate economica integrata pietei:",
+        "Realizeaza activitati necomerciale",
+        "Colecteaza taxe si impozite",
+        "Mobilizeaza disponibilitatile banesti din economie",
+        "Creeaza numerar",
+    3],
+    // 24
+    ["Valoarea adaugata creata in economie reprezinta:",
+        "Consum intermediar",
+        "Capital circulant",
+        "Diferenta dintre cifra de afaceri si investitii",
+        "Plusul de valoare care se creeaza in intreprinderi",
+    4],
+    // 25
+    ["Daca o intreprindere angajeaza lucratori, la nivel economic se realizeaza:",
+        "Flux monetar",
+        "Flux real si monetar",
+        "Nu se poate vorbi de existenta unui flux real / monetar pe piata muncii",
+        "Activitati necomerciale",
+    2],
+    // 26
+    ["Finalitatea activitatii economice libere este reprezentata de:",
+        "Satisfacerea nevoilor consumatorilor",
+        "Multiplicarea surselor de venit ale statului",
+        "Imbogatirea agentilor economici",
+        "Corectarea imperfectiunilor pietei",
+    1],
+    // 27
+    ["Interventia statului in sistemul economic liber este indreptata in directia:",
+        "Imbogatirii unor reprezentanti ai statului",
+        "Eficientizarii activitatii agentilor economici individuali",
+        "Satisfacerii unor nevoi colective",
+        "Centralizarii activitatii economice",
+    3],
+    // 28
+    ["In economia de piata, bancile ca agenti ai creditarii efectueaza activitati:",
+        "Comerciale",
+        "Necomerciale pentru populatie",
+        "Necomerciale pentru administratia publica",
+        "De producere a bunurilor economice si prestare servicii",
+    1],
+    // 29
+    ["In economia concurentiala, instrainarea integrala a activitatii se realizeaza:",
+        "Prin arendare",
+        "Prin inchiriere",
+        "Exclusiv prin vanzare-cumparare",
+        "Prin mostenire",
+    4],
+    // 30
+    ["Obiectivul urmarit de agentii economici care desfasoara activitati este:",
+        "Majorarea veniturilor statului",
+        "Maximizarea profitului",
+        "Multiplicarea consumului intermediar",
+        "Cresterea taxelor aplicate fiecarei activitati",
+    2], 
+];
+
+var question = document.getElementById('question');
+var answer1 = document.getElementById('answer-1-content');
+var answer2 = document.getElementById('answer-2-content');
+var answer3 = document.getElementById('answer-3-content');
+var answer4 = document.getElementById('answer-4-content');
+
+function loadQuestion() {
+    question.innerHTML = data[questionIndex-1][0];
+    answer1.innerHTML = data[questionIndex-1][1];
+    answer2.innerHTML = data[questionIndex-1][2];
+    answer3.innerHTML = data[questionIndex-1][3];
+    answer4.innerHTML = data[questionIndex-1][4];
+}
